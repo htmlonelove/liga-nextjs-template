77012d316ba8d0d34b593770249b8c2a2c48ccdd
Andrey Pankov <mateelperm@gmail.com>
2026-05-20 21:01:25 +0500

    chore(deps): migrate inquirer to @inquirer/prompts 8

--- util/component/gen-component.mjs
+++ util/component/gen-component.mjs
@@ -1,83 +1,60 @@
 import fs from 'fs'
-import inquirer from 'inquirer'
-import inquirerFileTreeSelection from 'inquirer-file-tree-selection-prompt'
+import { input, select } from '@inquirer/prompts'
 import camelcase from 'camelcase'
-import {indexTemplate, tsTemplate, tsxTemplate, scssTemplate} from './template.mjs'
-import {createIndex} from './gen-index.mjs'
-import path from 'path'
+import { indexTemplate, tsTemplate, tsxTemplate, scssTemplate } from './template.mjs'
+import { createIndex } from './gen-index.mjs'
 import chalk from 'chalk'
-
-inquirer.registerPrompt('file-tree-selection', inquirerFileTreeSelection)
 
 const DIR_NAMES = ['views', 'modules', 'components', 'ui']
 
-inquirer
-  .prompt([
-    {
-      name: 'componentName',
-      message: chalk.blueBright('Введите название компонента:')
-    },
-    {
-      name: 'componentFolder',
-      message: chalk.blueBright('Выберите директорию:'),
-      type: 'file-tree-selection',
-      root: './src',
-      onlyShowDir: true,
-      onlyShowValid: true,
-      validate: (input) => {
-        const dirName = input.split(path.sep).pop()
-        return DIR_NAMES.includes(dirName)
-      },
-      transformer: (input) => {
-        return input.split(path.sep).pop()
-      }
+const componentName = await input({
+  message: chalk.blueBright('Введите название компонента:')
+})
+
+const dirName = await select({
+  message: chalk.blueBright('Выберите директорию:'),
+  choices: DIR_NAMES
+})
+
+const componentFolder = `./src/${dirName}`
+const camelCaseName = camelcase(componentName)
+const isView = dirName === 'views'
+const isModule = dirName === 'modules'
+
+console.log(chalk.greenBright('📜 Используем шаблон компонента с названием: ' + chalk.whiteBright(camelCaseName)))
+
+const componentDirectory = `${componentFolder}/${camelCaseName}`
+
+if (fs.existsSync(componentDirectory)) {
+  console.error(chalk.red(`❌ Компонет ${chalk.whiteBright(camelCaseName)} уже существует`))
+  process.exit(1)
+}
+
+fs.mkdirSync(componentDirectory, {
+  recursive: true
+})
+
+const generatedTemplates = [tsTemplate, scssTemplate, tsxTemplate, indexTemplate].map((template) => {
+  if (isView) {
+    switch (template.name) {
+      case 'tsxTemplate':
+        return template(camelCaseName, true)
+      case 'indexTemplate':
+        return template(camelCaseName, 'View')
     }
-  ])
-  .then((answers) => {
-    const { componentName, componentFolder } = answers
-    const dirName = componentFolder.split(path.sep).pop()
-    const camelCaseName = camelcase(componentName)
-    const isView = dirName === 'views'
-    const isModule = dirName === 'modules'
+  }
+  return template(camelCaseName)
+})
 
-    console.log(chalk.greenBright('📜 Используем шаблон компонента с названием: ' + chalk.whiteBright(camelCaseName)))
+generatedTemplates.forEach((template) => {
+  fs.writeFileSync(
+    `${componentDirectory}/${template.filename}`,
+    template.content
+  )
+})
 
-    const componentDirectory = `${componentFolder}/${camelCaseName}`
+if (!isView && !isModule) {
+  createIndex(componentFolder, componentName)
+}
 
-    if (fs.existsSync(componentDirectory)) {
-      console.error(chalk.red(`❌ Компонет ${chalk.whiteBright(camelCaseName)} уже существует`))
-      process.exit(1)
-    }
-
-    if (!fs.existsSync(componentDirectory)) {
-      fs.mkdirSync(componentDirectory, {
-        recursive: true
-      })
-    }
-
-    const generatedTemplates = [tsTemplate, scssTemplate, tsxTemplate, indexTemplate].map((template) => {
-      if (isView) {
-        switch (template.name) {
-          case 'tsxTemplate':
-            return template(camelCaseName, true)
-          case 'indexTemplate':
-            return template(camelCaseName, 'View')
-        }
-      }
-      return template(camelCaseName)
-    })
-
-    generatedTemplates.forEach((template) => {
-      fs.writeFileSync(
-        `${componentDirectory}/${template.filename}`,
-        template.content
-      )
-    })
-
-    if (!isView && !isModule) {
-      createIndex(componentFolder, componentName)
-    }
-
-    const dirPath = `./src/${dirName}`
-    console.log(`✅  ${chalk.greenBright(`Создан компонент`)} ${chalk.whiteBright(camelcase(componentName))} ${chalk.greenBright('в директории')} ${chalk.whiteBright(dirPath)}`)
-  })
+console.log(`✅  ${chalk.greenBright(`Создан компонент`)} ${chalk.whiteBright(camelCaseName)} ${chalk.greenBright('в директории')} ${chalk.whiteBright(componentFolder)}`)
